2ce1e205417e74b48387c819b72867501e5bdffe
Vidya <107308948+KattekolaSreeVidya@users.noreply.github.com>
2024-07-12 11:12:02 +0530

    Add files via upload
    Check Pair

---
+++ Arraysss.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class Arraysss{
+    static boolean checkPair(int[] arr,int x){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==x){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        int[] arr={1,2,3,4,5};
+        int x=99;
+        if (checkPair(arr,x)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
